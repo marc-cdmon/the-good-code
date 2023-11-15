b40aea8565e2e9896b5b9feee52c6bbb5bbf682c
Marc Palau <127146270+marc-cdmon@users.noreply.github.com>
2023-11-15 16:11:32 +0100

    Create test003_package.js

---
+++ test003_package.js
@@ -0,0 +1,14 @@
+{
+  "name": "simple-node-app",
+  "version": "1.0.0",
+  "description": "A simple Node.js app for Docker demonstration",
+  "main": "index.js",
+  "scripts": {
+    "start": "node index.js"
+  },
+  "dependencies": {
+    "express": "^4.17.1"
+  },
+  "author": "",
+  "license": "ISC"
+}
